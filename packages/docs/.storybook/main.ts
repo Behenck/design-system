a03f98aa4f8d9b19c5d5f0ef7af47957e3a17cb1
Behenck <denilsontrespa10@gmail.com>
2023-11-21 22:44:53 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -2,10 +2,6 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 import { join, dirname } from "path";
 
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
 function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
@@ -25,5 +21,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system'
+    }
+
+    return config
+  }
 };
 export default config;
